bb0677bf45e7ab159e0e277f84c9da60a217737c
jiikko <n905i.1214@gmail.com>
2019-09-23 19:25:13 +0900

    Initial Commit

---
+++ sample-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-ios-app
+//
+//  Created by KOJI KAWAGUCHI on 2019/09/23.
+//  Copyright © 2019 KOJI KAWAGUCHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
